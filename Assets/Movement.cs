e493584e04aed1a03c3cd957080d83f516fc5f12
Arbonik <zmear@mail.ru>
2020-08-20 17:00:09 +0700

    init

---
+++ Assets/Movement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public int speed = 10;
+
+    Rigidbody rigidbody;
+    Vector3 move;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        move.Set(horizontal, 0f, vertical);
+        rigidbody.MovePosition(rigidbody.position + move * speed * Time.deltaTime);
+    }
+}
